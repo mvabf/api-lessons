d0c46231662d983903690ea95f78d39440dcb7ba
Marcus Baltazar <mvabf_dev@outlook.com>
2022-01-18 19:15:33 -0300

    feat: crud classes finished

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -10,12 +10,13 @@ class ClassesController {
         const newClass = { name, description, video, data_init, data_end };
 
         try {
-            await ClassSchema.create(newClass);
+            const createdClass = await ClassSchema.create(newClass);
+
+            return res.status(201).json(createdClass);
+
         } catch (err) {
             return res.status(404).json({ error: err });
         }
-
-        return res.json(newClass);
     }
 
     public async list(req: Request, res: Response): Promise<Response> {
@@ -36,11 +37,11 @@ class ClassesController {
                 video: cl.video,
                 data_init: cl.data_init,
                 data_end: cl.data_end,
-                data_created: cl.data_created,
-                data_updated: cl.data_updated,
                 total_comments: cl.total_comments,
-                last_comment: classLastComment.comment,
-                last_comment_date: classLastComment.date_created
+                last_comment: classLastComment != null ? classLastComment.comment : '',
+                last_comment_date: classLastComment != null ? classLastComment.date_created : '',
+                date_created: cl.date_created,
+                date_updated: cl.date_updated
             };
 
             listOfClasses.push(tindinClass);
@@ -50,6 +51,54 @@ class ClassesController {
             || cl.description.includes(data_init) || cl.description.includes(data_end)) : listOfClasses;
         
         return res.json(listOfClassesFilter);
+    }
+
+    public async find(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params;
+
+        try {
+            const findClass = await ClassSchema.findById(id); 
+
+            const classComments = await CommentSchema.find().limit(3).sort('-date_created');
+
+            const classReturn = {
+                name: findClass.name,
+                comments: classComments.map(c => { return c.comment})
+            }
+
+            return res.json(classReturn);
+
+        } catch(err) {
+            return res.status(404).json({error: 'id not found!'});
+        }
+    }
+
+    public async update(req: Request, res: Response): Promise<Response> {
+        const { id, name, description, video, data_init, data_end } = req.body;
+
+        try {
+            const classToUpdate = await ClassSchema.findByIdAndUpdate({ _id: id }, {
+            name, description, video, data_init, data_end
+            });
+
+            return res.json(classToUpdate);   
+            
+        } catch(err) {
+            return res.status(404).json();
+        }
+    }
+
+    public async delete(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params;
+
+        try {
+            await ClassSchema.findByIdAndDelete(id);
+
+            return res.status(204).json({});
+
+        } catch (err) {
+            return res.status(404).json({ error: 'id not found!' });
+        }
     }
 }
 
--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -8,6 +8,12 @@ const classRouter = Router();
 
 classRouter.post('/', authMiddleware, ClassesController.create);
 classRouter.get('/', authMiddleware, ClassesController.list);
+classRouter.put('/', authMiddleware, ClassesController.update)
+classRouter.delete('/:id', authMiddleware, ClassesController.delete);
+classRouter.get('/:id', authMiddleware, ClassesController.find);
+
+classRouter.get('/comments');
+classRouter.delete('/comments/:id');
 classRouter.post('/comments', authMiddleware, CommentsController.create);
 
 
